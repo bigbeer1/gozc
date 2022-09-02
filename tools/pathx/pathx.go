0a8b3f0be90112d3f8fe1f5c00e1b1a8ee0f7e48
dabenxiong <747321301@qq.com>
2022-09-02 14:28:05 +0800

    fix(解决莫名其妙创建一个文件夹gen的问题): 解决莫名其妙创建一个文件夹gen的问题
    解决莫名其妙创建一个文件夹gen的问题

--- tools/pathx/pathx.go
+++ tools/pathx/pathx.go
@@ -45,19 +45,6 @@ func GetTemplateDir(category string) (string, error) {
 	return filepath.Join(env, category), nil
 }
 
-// MkdirIfNotExist makes directories if the input path is not exists
-func MkdirIfNotExist(dir string) error {
-	if len(dir) == 0 {
-		return nil
-	}
-
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		return os.MkdirAll(dir, os.ModePerm)
-	}
-
-	return nil
-}
-
 const (
 	NL              = "\n"
 	goctlDir        = ".goctl"
